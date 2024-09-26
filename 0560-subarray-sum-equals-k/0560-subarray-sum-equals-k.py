83e335ba6e77408df65a75d2802d8d9938732441
SeonWoo05 <164982026+SeonWoo05@users.noreply.github.com>
2024-09-26 12:34:36 +0900

    Time: 220 ms (74.29%), Space: 14.1 MB (76.03%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,18 +1,19 @@
 class Solution(object):
     def subarraySum(self, nums, k):
-        left = 0
-        sum = 0
-        cnt = 0
-        for i in range(len(nums)):
-            sum += nums[i]
-
-            while sum > k:
-                sum -= nums[left]
-                left += 1
-
-            if sum == k:
-                cnt += 1
-                sum -= nums[left]
-                left += 1
-
-        return cnt
+        sum_counts = {0: 1}  # 부분합이 0인 경우 1번 카운트 (빈 배열의 경우 처리)
+        curr_sum = 0  # 현재까지의 누적합
+        cnt = 0  # 부분 배열의 개수
+        
+        for num in nums:
+            curr_sum += num  # 현재 값을 더해 누적합을 구함
+            
+            # 현재 누적합에서 k를 뺀 값이 해시맵에 있으면, 그 수만큼 부분 배열이 존재함
+            if (curr_sum - k) in sum_counts:
+                cnt += sum_counts[curr_sum - k]
+            
+            # 현재 누적합을 해시맵에 저장하여 이후 부분 배열 계산에 사용
+            sum_counts[curr_sum] = sum_counts.get(curr_sum, 0) + 1
+                # sum_counts에서 curr_sum이라는 키를 찾아 curr_sum이 등장한 횟수를 가져옴
+                # 두번째 인자는 curr_sum이라는 키가 없으면 0을 반환
+        
+        return cnt                
